1b2c64d15d19d923e7e4393a8b83eaa218fe4bd5
Connor Langlois <connor.langlois@gmail.com>
2019-04-15 18:19:45 -0400

    Update wolf.py

--- src/wolf.py
+++ src/wolf.py
@@ -2,22 +2,30 @@ import pandas as pd
 
 from os import path
 from numpy import random
+from sklearn.preprocessing import StandardScaler
 from keras.models import Sequential
 from keras.layers import LSTM
+from keras.preprocessing.sequence import TimeseriesGenerator
 
 random.seed(0)
 
 df = pd.read_csv(path.relpath('data/aapl.csv'))[::-1]
-x = df.loc[:, df.columns != 'Correction'].values
+features = df.columns.difference(['Date', 'Correction'])
+
+x = StandardScaler().fit_transform(df[features].astype(float))
 y = df['Correction'].values
 
-print(df[df.columns != 'Correction'])
-print(x, y)
+series = TimeseriesGenerator(x, y, 14, batch_size=len(x))[0]
+x_series = series[0]
+y_series = series[1]
 
 train_p = 0.90
 index = int(train_p * len(df))
-train = df[:index]
-test = df[index:]
+
+train_x = x_series[:index]
+train_y = y_series[:index]
+test_x = x_series[index:]
+test_y = y_series[index:]
 
 model = Sequential()
 model.add(LSTM(10))
