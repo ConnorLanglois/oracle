aa25804fbfe3ce96b491c4d198cf567f2ea23be5
Connor Langlois <connor.langlois@gmail.com>
2019-05-03 22:37:41 -0400

    Add GE data and test runs, update wolf

--- src/wolf.py
+++ src/wolf.py
@@ -1,16 +1,26 @@
-from pathlib import Path
+import os
+import sys
 
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+import tensorflow as tf
+
+stderr = sys.stderr
+sys.stderr = open(os.devnull, 'w')
+
 from keras.models import Sequential
-from keras.layers import BatchNormalization, Dense, LSTM
+from keras.layers import BatchNormalization, Dense, LSTM, GRU
 from keras.preprocessing.sequence import TimeseriesGenerator
 
+sys.stderr = stderr
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+tf.logging.set_verbosity(tf.logging.ERROR)
 np.random.seed(0)
 
-df = pd.read_csv(Path() / 'data/aapl.csv')[::-1]
+df = pd.read_csv('data/aapl.csv')[::-1]
 
 x = StandardScaler().fit_transform(df[df.columns.difference(['Date', 'Correction'])].astype(float))
 y = df['Correction'].values
@@ -19,24 +29,21 @@ series = TimeseriesGenerator(x, y, 14, batch_size=len(x))[0]
 series_x = series[0]
 series_y = series[1]
 
-train_p = 0.90
-index = int(train_p * len(df))
-
-train_x = series_x[:index]
-train_y = series_y[:index]
-test_x = series_x[index:]
-test_y = series_y[index:]
+train_x, test_x, train_y, test_y = train_test_split(series_x, series_y, test_size=0.1, shuffle=False)
 
 model = Sequential()
 model.add(LSTM(10, activation='relu'))
 model.add(BatchNormalization())
 model.add(Dense(1, activation='sigmoid'))
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(train_x, train_y, epochs=100)
+model.compile(loss='binary_crossentropy', optimizer='adam', shuffle=False, metrics=['accuracy'])
 
-metrics = model.evaluate(test_x, test_y)
-predictions = model.predict(test_x)
+for i in range(1, 301):
+	model.fit(train_x, train_y, epochs=1, verbose=0)
 
-print('\nAccuracy: ', metrics[1])
-print('\nPredictions:\n', predictions)
+	train_acc = model.evaluate(train_x, train_y, verbose=0)[1]
+	test_acc = model.evaluate(test_x, test_y, verbose=0)[1]
+
+	print('Epoch:', i)
+	print('Train Acc:', round(train_acc, 2))
+	print('Test Acc:', round(test_acc, 2), end='\n\n')
